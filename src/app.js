cd5ed65450409384456ae0ec4386eb9d5253add2
Dimitrios Chatziioannou <threequartersdev@gmail.com>
2020-06-29 18:39:01 +0100

    added gsap scroll

--- src/app.js
+++ src/app.js
@@ -1,3 +1,31 @@
 import './scss/app.scss';
 
-import Animations from './components/Animantions';
+import { gsap } from 'gsap';
+import { CSSRulePlugin } from 'gsap/CSSRulePlugin';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(CSSRulePlugin, ScrollToPlugin, ScrollTrigger);
+
+const projects = document.querySelector('.projects');
+const about = document.querySelector('.about');
+const contact = document.querySelector('.contact');
+
+projects.addEventListener('click', () => {
+  gsap.to(window, {
+    duration: 1,
+    scrollTo: { y: '#projects', offsetY: 100 },
+  });
+});
+about.addEventListener('click', () => {
+  gsap.to(window, {
+    duration: 1,
+    scrollTo: { y: '#about' },
+  });
+});
+contact.addEventListener('click', () => {
+  gsap.to(window, {
+    duration: 1,
+    scrollTo: { y: '#contact', offsetY: 100 },
+  });
+});
--- src/components/Animantions.js
+++
@@ -1,6 +0,0 @@
-import { gsap } from 'gsap';
-import { CSSRulePlugin } from 'gsap/CSSRulePlugin';
-import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-
-gsap.registerPlugin(CSSRulePlugin, ScrollToPlugin, ScrollTrigger);
